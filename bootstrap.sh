b8155b03f1d55de31451cca15e6ec1b1ba36635b
Yevgene-DP <ebalalay@gmail.com>
2025-06-25 20:46:59 +0300

    Update files pv.yml and bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-kubectl apply -f namespace.yml
-kubectl apply -f configMap.yml
-kubectl apply -f secret.yml
-kubectl apply -f pv.yml
-kubectl apply -f pvc.yml
-kubectl apply -f deployment.yml
+kubectl apply -f .infrastructure/namespace.yml
+kubectl apply -f .infrastructure/configMap.yml
+kubectl apply -f .infrastructure/secret.yml
+kubectl apply -f .infrastructure/pv.yml
+kubectl apply -f .infrastructure/pvc.yml
+kubectl apply -f .infrastructure/deployment.yml
